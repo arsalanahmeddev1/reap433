530bf591c4e71e729f1f7342074d995fdc48dcce
unknown <salman@chameleon-ideas.com>
2026-09-01 00:42:39 +0500

    check scoring

--- app/Http/Controllers/Api/AnswerController.php
+++ app/Http/Controllers/Api/AnswerController.php
@@ -80,12 +80,11 @@ class AnswerController extends ApiController
         $correctAnswers = (int) (clone $attemptsQuery)->where('answer_is_right', 1)->count();
 
         $accuracyPercent = (int) round(($correctAnswers / $totalQuestion) * 100);
-        $score = $answerXp / $totalQuestion;
 
         $attemptsQuery->update(['is_complete' => 1]);
 
         return $this->success([
-            'score' => $answerXp + '/' + $totalQuestion,
+            'score' => $answerXp.'/'.$totalQuestion,
             'answer_xp' => $answerXp,
             'answer_coins' => $answerCoins,
             'total_question' => $totalQuestion,
